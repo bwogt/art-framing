95417396128a4885415849067d5504ed987a9acd
Bruno José dos Santos Wogt <bwogt.dev@gmail.com>
2025-08-25 15:53:10 -0300

    @bruw/6/feat/services (#7)
    * service section created
    
    * default style
    
    * header title
    
    * navbar custom
    
    * fonts fix

--- assets/js/navbar-animation.js
+++ assets/js/navbar-animation.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbarToggler = document.querySelector(".navbar-toggler");
     const animatedIcon = document.querySelector(".animated-icon");
+    const navbar = document.querySelector('.navbar');
+    const navbarContainer = document.querySelector('.navbar .container');
 
     navbarToggler.addEventListener("click", function () {
         animatedIcon.classList.toggle("open");
+    });
+
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 50) {
+            navbar.classList.add('navbar-scrolled');
+            navbarContainer.classList.add('no-border');
+        } else {
+            navbar.classList.add('navbar-custom');
+            navbar.classList.remove('navbar-scrolled');
+            navbarContainer.classList.remove('no-border');
+        }
     });
 });
