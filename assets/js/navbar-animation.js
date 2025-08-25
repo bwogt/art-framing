a2dc43a9976ff1cd7f55265962ac9145617ad3cb
Bruno José dos Santos Wogt <bwogt.dev@gmail.com>
2025-08-25 08:22:35 -0300

    @bruw/1/feat/header (#2)
    * added bootstrap
    
    * Created header and navbar
    
    * folder organization
    
    * removed unused images

---
+++ assets/js/navbar-animation.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarToggler = document.querySelector(".navbar-toggler");
+    const animatedIcon = document.querySelector(".animated-icon");
+
+    navbarToggler.addEventListener("click", function () {
+        animatedIcon.classList.toggle("open");
+    });
+});
